fixed bug in gen_calib_board for rotation

simulation/camera.py
@@ -91,16 +91,20 @@ class Camera:
 		cam_str += str(self.size)
 		return cam_str
 
+	@staticmethod
+	def make_pinhole_camera():
+		"""
+		Make a camera whose intrinsics and distortion coefficients are computed 
+		from a real experiment.
 
-def make_pinhole_camera():
-	"""
-	Make a camera with given intrinsics and distortion coefficients
-	"""
-	intri_mat = np.array([[6.1578617661522037e+02, 0., 6.1058715036731826e+02], \
-		[0., 6.1238350456764329e+02, 5.2959938173888008e+02],\
-		[0., 0., 1.]])
-	radial_dist = np.array([-1.8871087691827788e-02, 3.1035356528952687e-02, -1.2440956825625479e-02])
-	tang_dist = np.array([1.1627819998974306e-03, -1.8172149748173956e-04])
-	intri = Intrinsics(intri_mat, radial_dist, tang_dist)
+		Returns:
+			a Camera.
+		"""
+		intri_mat = np.array([[6.1578617661522037e+02, 0., 6.1058715036731826e+02], \
+			[0., 6.1238350456764329e+02, 5.2959938173888008e+02],\
+			[0., 0., 1.]])
+		radial_dist = np.array([-1.8871087691827788e-02, 3.1035356528952687e-02, -1.2440956825625479e-02])
+		tang_dist = np.array([1.1627819998974306e-03, -1.8172149748173956e-04])
+		intri = Intrinsics(intri_mat, radial_dist, tang_dist)
 
-	return Camera(intri, None, (1264, 1016),"pinhole")
+		return Camera(intri, None, (1264, 1016),"pinhole")

simulation/exp_util.py
@@ -13,111 +13,141 @@ import math
 #         self.loc = location
 #         self.id_num = id_num
 
-def euler2mat(e_angles):
-    """
-    Returns the rotation matrix for given Euler angle (rx, ry, rz)
-    Args:
-        e_angles: 1x3 numpy array
+# def euler2mat(e_angles):
+#     """
+#     Returns the rotation matrix for given Euler angle (rx, ry, rz)
+#     Args:
+#         e_angles: 1x3 numpy array
 
-    Returns:
-        rot_mat: rotation matrix of given euler angles
-    """
-    rot_mat = np.eye(3)
-    cosz = math.cos(e_angles[2])
-    sinz = math.sin(e_angles[2])
-    zmat = np.array([[cosz, -sinz, 0], [sinz, cosz, 0],[0, 0, 1]])
+#     Returns:
+#         rot_mat: rotation matrix of given euler angles
+#     """
+#     rot_mat = np.eye(3)
+#     cosz = math.cos(e_angles[2])
+#     sinz = math.sin(e_angles[2])
+#     zmat = np.array([[cosz, -sinz, 0], [sinz, cosz, 0],[0, 0, 1]])
 
-    cosy = math.cos(e_angles[1])
-    siny = math.sin(e_angles[1])
-    ymat = np.array([[cosy, 0, siny], [0, 1, 0], [-siny, 0, cosy]])
+#     cosy = math.cos(e_angles[1])
+#     siny = math.sin(e_angles[1])
+#     ymat = np.array([[cosy, 0, siny], [0, 1, 0], [-siny, 0, cosy]])
 
-    cosx = math.cos(e_angles[0])
-    sinx = math.sin(e_angles[0])
-    xmat = np.array([[1, 0, 0], [0, cosx, -sinx], [0, sinx, cosx]])
+#     cosx = math.cos(e_angles[0])
+#     sinx = math.sin(e_angles[0])
+#     xmat = np.array([[1, 0, 0], [0, cosx, -sinx], [0, sinx, cosx]])
 
-    return np.dot(xmat, np.dot(ymat, zmat))
+#     return np.dot(xmat, np.dot(ymat, zmat))
 
-def mat2euler(M, cy_thresh=None):
-    ''' Discover Euler angle vector from 3x3 matrix
+# def mat2euler(M, cy_thresh=None):
+#     ''' Discover Euler angle vector from 3x3 matrix
 
-    Uses the conventions above.
+#     Uses the conventions above.
 
-    Parameters
-    ----------
-    M : array-like, shape (3,3)
-    cy_thresh : None or scalar, optional
-       threshold below which to give up on straightforward arctan for
-       estimating x rotation.  If None (default), estimate from
-       precision of input.
+#     Parameters
+#     ----------
+#     M : array-like, shape (3,3)
+#     cy_thresh : None or scalar, optional
+#        threshold below which to give up on straightforward arctan for
+#        estimating x rotation.  If None (default), estimate from
+#        precision of input.
 
-    Returns
-    -------
-    z : scalar
-    y : scalar
-    x : scalar
-       Rotations in radians around z, y, x axes, respectively
+#     Returns
+#     -------
+#     z : scalar
+#     y : scalar
+#     x : scalar
+#        Rotations in radians around z, y, x axes, respectively
 
-    Notes
-    -----
-    If there was no numerical error, the routine could be derived using
-    Sympy expression for z then y then x rotation matrix, which is::
+#     Notes
+#     -----
+#     If there was no numerical error, the routine could be derived using
+#     Sympy expression for z then y then x rotation matrix, which is::
 
-      [                       cos(y)*cos(z),                       -cos(y)*sin(z),         sin(y)],
-      [cos(x)*sin(z) + cos(z)*sin(x)*sin(y), cos(x)*cos(z) - sin(x)*sin(y)*sin(z), -cos(y)*sin(x)],
-      [sin(x)*sin(z) - cos(x)*cos(z)*sin(y), cos(z)*sin(x) + cos(x)*sin(y)*sin(z),  cos(x)*cos(y)]
+#       [                       cos(y)*cos(z),                       -cos(y)*sin(z),         sin(y)],
+#       [cos(x)*sin(z) + cos(z)*sin(x)*sin(y), cos(x)*cos(z) - sin(x)*sin(y)*sin(z), -cos(y)*sin(x)],
+#       [sin(x)*sin(z) - cos(x)*cos(z)*sin(y), cos(z)*sin(x) + cos(x)*sin(y)*sin(z),  cos(x)*cos(y)]
 
-    with the obvious derivations for z, y, and x
+#     with the obvious derivations for z, y, and x
 
-       z = atan2(-r12, r11)
-       y = asin(r13)
-       x = atan2(-r23, r33)
+#        z = atan2(-r12, r11)
+#        y = asin(r13)
+#        x = atan2(-r23, r33)
 
-    Problems arise when cos(y) is close to zero, because both of::
+#     Problems arise when cos(y) is close to zero, because both of::
 
-       z = atan2(cos(y)*sin(z), cos(y)*cos(z))
-       x = atan2(cos(y)*sin(x), cos(x)*cos(y))
+#        z = atan2(cos(y)*sin(z), cos(y)*cos(z))
+#        x = atan2(cos(y)*sin(x), cos(x)*cos(y))
 
-    will be close to atan2(0, 0), and highly unstable.
+#     will be close to atan2(0, 0), and highly unstable.
 
-    The ``cy`` fix for numerical instability below is from: *Graphics
-    Gems IV*, Paul Heckbert (editor), Academic Press, 1994, ISBN:
-    0123361559.  Specifically it comes from EulerAngles.c by Ken
-    Shoemake, and deals with the case where cos(y) is close to zero:
+#     The ``cy`` fix for numerical instability below is from: *Graphics
+#     Gems IV*, Paul Heckbert (editor), Academic Press, 1994, ISBN:
+#     0123361559.  Specifically it comes from EulerAngles.c by Ken
+#     Shoemake, and deals with the case where cos(y) is close to zero:
 
-    See: http://www.graphicsgems.org/
+#     See: http://www.graphicsgems.org/
 
-    The code appears to be licensed (from the website) as "can be used
-    without restrictions".
-    '''
-    M = np.asarray(M)
-    if cy_thresh is None:
-        try:
-            cy_thresh = np.finfo(M.dtype).eps * 4
-        except ValueError:
-            cy_thresh = _FLOAT_EPS_4
-    r11, r12, r13, r21, r22, r23, r31, r32, r33 = M.flat
-    # cy: sqrt((cos(y)*cos(z))**2 + (cos(x)*cos(y))**2)
-    cy = math.sqrt(r33*r33 + r23*r23)
-    if cy > cy_thresh: # cos(y) not close to zero, standard form
-        z = math.atan2(-r12,  r11) # atan2(cos(y)*sin(z), cos(y)*cos(z))
-        y = math.atan2(r13,  cy) # atan2(sin(y), cy)
-        x = math.atan2(-r23, r33) # atan2(cos(y)*sin(x), cos(x)*cos(y))
-    else: # cos(y) (close to) zero, so x -> 0.0 (see above)
-        # so r21 -> sin(z), r22 -> cos(z) and
-        z = math.atan2(r21,  r22)
-        y = math.atan2(r13,  cy) # atan2(sin(y), cy)
-        x = 0.0
-    return z, y, x
+#     The code appears to be licensed (from the website) as "can be used
+#     without restrictions".
+#     '''
+#     M = np.asarray(M)
+#     if cy_thresh is None:
+#         try:
+#             cy_thresh = np.finfo(M.dtype).eps * 4
+#         except ValueError:
+#             cy_thresh = _FLOAT_EPS_4
+#     r11, r12, r13, r21, r22, r23, r31, r32, r33 = M.flat
+#     # cy: sqrt((cos(y)*cos(z))**2 + (cos(x)*cos(y))**2)
+#     cy = math.sqrt(r33*r33 + r23*r23)
+#     if cy > cy_thresh: # cos(y) not close to zero, standard form
+#         z = math.atan2(-r12,  r11) # atan2(cos(y)*sin(z), cos(y)*cos(z))
+#         y = math.atan2(r13,  cy) # atan2(sin(y), cy)
+#         x = math.atan2(-r23, r33) # atan2(cos(y)*sin(x), cos(x)*cos(y))
+#     else: # cos(y) (close to) zero, so x -> 0.0 (see above)
+#         # so r21 -> sin(z), r22 -> cos(z) and
+#         z = math.atan2(r21,  r22)
+#         y = math.atan2(r13,  cy) # atan2(sin(y), cy)
+#         x = 0.0
+#     return z, y, x
+
+# def unit_vector(vector):
+#     """ 
+#     http://stackoverflow.com/questions/2827393/angles-between-two-n-dimensional
+#     -vectors-in-python
+#     Returns the unit vector of the vector.  
+#     """
+#     return vector / np.linalg.norm(vector)
+
+# def angle_between(v1, v2):
+#     """ 
+#     http://stackoverflow.com/questions/2827393/angles-between-two-n-dimensional
+#     -vectors-in-python
+#     Returns the angle in radians between vectors 'v1' and 'v2'::
+
+#             >>> angle_between((1, 0, 0), (0, 1, 0))
+#             1.5707963267948966
+#             >>> angle_between((1, 0, 0), (1, 0, 0))
+#             0.0
+#             >>> angle_between((1, 0, 0), (-1, 0, 0))
+#             3.141592653589793
+#     """
+#     v1_u = unit_vector(v1)
+#     v2_u = unit_vector(v2)
+#     angle = np.arccos(np.dot(v1_u, v2_u))
+#     if np.isnan(angle):
+#         if (v1_u == v2_u).all():
+#             return 0.0
+#         else:
+#             return np.pi
+#     return angle
 
 def rotation_matrix(axis, theta):
     """    
     Return the rotation matrix associated with counterclockwise rotation about
     the given axis by theta radians.
 
-    From: http://stackoverflow.com/questions/6802577/python-rotation-of-3d-vector
+    From: http://stackoverflow.com/questions/6802577/python-rotation-of-3d-
+    vector
     """
-    axis = np.asarray(axis)
-    theta = np.asarray(theta)
     axis = axis/math.sqrt(np.dot(axis, axis))
     a = math.cos(theta/2.0)
     b, c, d = -axis*math.sin(theta/2.0)
@@ -156,13 +186,19 @@ def gen_calib_board(board_height, board_width, sqsize, \
     pt_id = 0
     for x in xrange(board_height):
         for y in xrange(board_width):
-            board[pt_id] = np.array([x * sqsize, y * sqsize, 1], np.float32)
+            board[pt_id] = np.array([x * sqsize, y * sqsize, 0], np.float32)
             pt_id += 1
 
     # Rotate board to given orientation and move to given location
-    rot_mat = euler2mat(orientation - np.array([0,0,1]))
-    for pt in board.keys():
-        board[pt] = rot_mat.dot(board[pt]) + location
+    z_vec = np.array([0,0,1])
+    rot_axis = np.cross(z_vec, orientation)
+    r_angle = np.arccos( np.dot(z_vec,orientation)/np.linalg.norm(orientation) ) #signed?
+    # print "rot_axis", rot_axis, "r_angle", r_angle
+    if not np.isnan(r_angle):
+        rot_mat = rotation_matrix(rot_axis, r_angle)
+        # print "rot_mat:", rot_mat
+        for pt in board.keys():
+            board[pt] = rot_mat.dot(board[pt]) + location
 
     # Add noise3d
     if noise3d > 0: