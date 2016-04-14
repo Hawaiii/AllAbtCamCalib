a8b1a0776fcdd0041d0474408146212478bac043
Hawaiii <yihua25@gmail.com>
2016-04-13 22:01:42 -0400

    refactoring

---
+++ simulation/board.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Board:
+	size = None #(board_width, board_height)
+	pts = {} # a dictionry keyed by control point positions (col, row)
+
+	def gen_calib_board():
+		#@TODO
+		print 'gen_calib_board not implemented!'
+
+	def move_board():
+		#TODO
+		print 'move_board not implemented!'
+
+	def get_four_corners():
+		#@TODO
+		print 'get_four_corners not implemented!'
+
+	def board_dict2array(board, board_dim):
+		#@TODO
+		print 'board_dict2array not implemented!'
+
+	
--- simulation/kalibr_data.py
+++ simulation/kalibr_data.py
@@ -15,34 +15,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 board_dim = (8, 5) #DONT CHANGE as this is the dimension of data/chess.png
-board = util.gen_calib_board(board_dim[0],board_dim[1],23, np.asarray([[0,0,600]]), np.zeros([1,3]), 0)
-
-# ax = vis.plot_poses(cam_motion, invert = True)
-# ax = vis.plot_calib_boards( [ board ], (7,8), fax=ax)
-# ax.set_aspect('equal')
-# plt.show()
+board = util.gen_calib_board(board_dim[0], board_dim[1], 23, np.asarray([[0,0,600]]), np.zeros([1,3]), 0)
 
 camera = cam.Camera.make_pinhole_camera()
 camera.intrinsics.radial_dist = np.zeros((1,3))
 camera.intrinsics.tang_dist = np.zeros((1,2))
 
 cam_motion = imu.spiral_motion(board, board_dim, camera)
-
-# Generate camera images
-# board_img = cv2.imread('data/chess.png')
-# Hs = camera.calc_homography(cam_motion, board, board_dim)
-# for i in xrange(len(Hs)):
-# 	targets.render_chessboard_homo(board_img, Hs[i], camera.get_opencv_size(2), save_name='results/'+str(cam_motion[i].time_stamp)+'.png')
-
-# Relative pose from camera to imu
-rel_pose = cam.Extrinsics.init_with_rotation_matrix(np.array([[-20.,0.,-10.]]), np.eye(3), time_stamp=None)
-
-# Generate imu motion
-imu_motion = imu.transform_motion(cam_motion, rel_pose)
-ax = vis.plot_poses(cam_motion, invert=True)
-ax = vis.plot_poses(imu_motion, invert=True, fax=ax)
+# ax = vis.plot_poses(cam_motion, invert = True)
 # ax = vis.plot_calib_boards( [ board ], (7,8), fax=ax)
-ax.set_aspect('equal')
-plt.show()
+# ax.set_aspect('equal')
+# plt.show()
 
-imu.gen_imu_readings(imu_motion, save_name='results/imu0.csv')
+
+# # Generate camera images
+# # board_img = cv2.imread('data/chess.png')
+# # Hs = camera.calc_homography(cam_motion, board, board_dim)
+# # for i in xrange(len(Hs)):
+# # 	targets.render_chessboard_homo(board_img, Hs[i], camera.get_opencv_size(2), save_name='results/'+str(cam_motion[i].time_stamp)+'.png')
+
+# # Relative pose from camera to imu
+# rel_pose = cam.Extrinsics.init_with_rotation_matrix(np.array([[-20.,0.,-10.]]), np.eye(3), time_stamp=None)
+
+# # Generate imu motion
+# imu_motion = imu.transform_motion(cam_motion, rel_pose)
+# ax = vis.plot_poses(cam_motion, invert=True)
+# ax = vis.plot_poses(imu_motion, invert=True, fax=ax)
+# # ax = vis.plot_calib_boards( [ board ], (7,8), fax=ax)
+# ax.set_aspect('equal')
+# plt.show()
+
+# imu.gen_imu_readings(imu_motion, save_name='results/imu0.csv')
