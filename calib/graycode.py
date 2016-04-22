ba2d62058ecf6571ffe227573971ee9312ce3989
Hawaiii <yhua1@andrew.cmu.edu>
2016-04-22 19:36:06 -0400

    starting graycode

---
+++ calib/graycode.py
@@ -0,0 +1,27 @@
+import cv2
+
+def make_greycode_seq(screen_res):
+    """
+    Returns a list of images (np array) to show and 
+    a dictionary where keys are graycode sequence and values 
+    are pixel locations.
+    Args:
+        screen_res: (screen_width, screen_height)
+    """
+    gc_seq = []
+    gc_dict = {}
+
+    # TODO
+
+    return gc_seq, gc_dict
+
+def show_im_seq(time_interval):
+    """
+    Shows images in a list on screen in order, each showing time_interval seconds.
+    """
+    #TODO
+
+gc_seq, gc_dict = make_greycode_seq((600,400))
+
+
+    
