c0572bf4fcafba411923cf544484d5a03889b723
erichuang0771 <ihuangyujia@me.com>
2016-04-24 18:14:08 -0400

    apple tree.h

--- CPP/AppleTree.h
+++ CPP/AppleTree.h
@@ -40,14 +40,34 @@ struct Chessboard {
 	}
 };
 
+struct Intrinsic {
+	//Intrinsics.. focus_X, focus_Y, pptx, ppty
+	double focus_x, focus_y, pptx, ppty;
+	Intrinsic(double f_x, double f_y, double p_x, double p_y):focus_x(f_x), focus_y(f_y),
+	pptx(p_x), ppty(p_y){}
+};
+
 class AppleJuice{
 	private:
 		Chessboard chessboard;
-
+		std::vector<std::vector<cv::Mat>> ImageLists;
+		std::vector<std::vector<cv::Mat>> BinaryImages;
+		std::vector<std::vector<cv::Point2f>> FeaturePool;
+		std::vector<std::vector<cv::Point3f>> PatternPtsPool;
 		cv::Point3f SearchPoints(std::string s);
 
+
 	public:
+		//read lookup table from txt >> Chessboard struct
 		void ReadLookup_table();
+		//read image from DIR >> ImageLists
+		void ReadImageLists();
+		//Biinarize all images >> BinaryImages
+		void BinarizeAllImages();
+		//Extract all control pts >> FeaturePools & PatternPtsPool 
+		void ExtractControlPts();
+		//Get Init. Guess from standart OpenCV Camera Calibration
+		void InitCameraCalibration();
 
 			
 		
